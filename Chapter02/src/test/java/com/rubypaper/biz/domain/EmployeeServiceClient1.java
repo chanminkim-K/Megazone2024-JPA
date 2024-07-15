a3cd8a6a0266bd6ac6d521ca890b1ad59c767eab
chanminkim-K <snoopy0813@naver.com>
2024-07-15 10:41:47 +0900

    [jpa] JPA로 새 테이블 create

---
+++ Chapter02/src/test/java/com/rubypaper/biz/domain/EmployeeServiceClient1.java
@@ -0,0 +1,53 @@
+package com.rubypaper.biz.domain;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.Date;
+
+/*
+ * JPA 관리한 Entity 생성
+ * - Entity 를 이용해서 데이터 삽입하는 테스트
+ *   1. EntityManagerFactory 이용해서 EntityManager를 생성
+ *   2. EntityManager 를 이용해서 영속성 관리
+ *      persist()
+ */
+
+class EmployeeServiceClient1 {
+
+    public static void main(String[] args) {
+
+        // <persistence-unit name="Chapter02"> 의 설정 정보를 참조
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("Chapter02");
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            // 영속성 관리 엔티티 생성
+            Employee1 employee1 = new Employee1();
+
+            employee1.setId(1L);
+            employee1.setName("홍길동");
+            employee1.setMailId("hong@naver.com");
+            employee1.setStartDate(new Date());
+            employee1.setTitle("대리");
+            employee1.setDeptName("개발부");
+            employee1.setSalary(2500.00);
+            employee1.setCommissionPct(12.50);
+
+            // 연속성 관리를 위한 엔티티 묶음
+            em.persist(employee1);
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+
+
+    }
+
+}
