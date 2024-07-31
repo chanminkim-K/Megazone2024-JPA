005cbcc16a65bdb6c130edc0b668d9c996356e81
chanminkim-K <snoopy0813@naver.com>
2024-07-31 22:58:00 +0900

    [JSP] Chapter09 - 웹 애플리케이션

--- RubyShopping/src/main/java/com/rubypaper/shopping/biz/domain/Order.java
+++ RubyShopping/src/main/java/com/rubypaper/shopping/biz/domain/Order.java
@@ -6,13 +6,15 @@ import lombok.ToString;
 import org.hibernate.exception.DataException;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "S_ORDER")
 @Data
 @NoArgsConstructor
-@ToString(exclude = {"customer", "searcgCustomerName", "searchOrderStatus"})
+@ToString(exclude = {"customer", "searcgCustomerName", "searchOrderStatus", "itemList"})
 public class Order {
 
     // 주문 아이디
@@ -42,17 +44,36 @@ public class Order {
     @Transient
     private OrderStatus searchOrderStatus;
 
+    // 주문내역 목록
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Item> itemList = new ArrayList<>();
+
     // 주문 생성자 : 주문한 회원 정보를 설정한다.
-    public Order(Customer customer) {
+    public Order(Customer customer, Item item) {
         setCustomer(customer);
-        this.customer = customer;
+        addItem(item);
         this.status = OrderStatus.ORDER; // 주문 생성 시 상태는 ORDER
         this.orderDate = new Date();
     }
+
 
     // 회원 설정 시에 회원 쪽에도 양방향 참조 설정
     public void setCustomer(Customer customer) {
         this.customer = customer;
         customer.getOrderList().add(this);
+    }
+
+    // 주문상품 설정 시에 주문상품 쪽에도 양방향 설정
+    private void addItem(Item item) {
+        itemList.add(item);
+        item.setOrder(this);
+    }
+
+    // 주문 취소 처리
+    public void orderCancel() {
+        this.setStatus(OrderStatus.CANCEL);
+        for (Item item : itemList) {
+            item.restoreStock();
+        }
     }
 }
